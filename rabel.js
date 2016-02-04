ec4765b70b8b8c2c27d23b0068c9d343b11dfcd5
Tim Berners-Lee <timbl+git@w3.org>
2016-02-03 19:31:36 -0500

    seems to work converting IANA XML file now. See text/xml:

--- rabel.js
+++ rabel.js
@@ -92,7 +92,7 @@ var doNext = function(remaining) {
             case '-inXML':
                 targetDocument = $rdf.sym($rdf.uri.join(right, base))
                 //console.log("Document is " + targetDocument)
-                readXML(targetDocument.uri,  {}, function(ok, body, xhr) {
+                readXML(targetDocument,  {}, function(ok, body, xhr) {
                     check(ok, body, xhr? xhr.status : undefined);
                     console.log("Loaded XML " + targetDocument);
                     doNext(remaining);
@@ -136,21 +136,183 @@ var doNext = function(remaining) {
     process.exit(0);
 }
 
-readXML = function(uri, options, callback) {
+//    Read An XML file
+//
+//   Contains namespace-trigged specials for  IANA registry data
+//
+readXML = function(targetDocument, options, callback) {
+    var uri = targetDocument.uri
     var file =  $rdf.Util.uri.refTo(base, uri);
-    fs.readFile(file, undefined, function(err,data){
-        if (err) return callback(false, err);
-        var ns = options.ns || $rdf.uri.join(file, base)
+    var ignore = { 7: true};
+    var RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+    var RDFS = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#');
+    
+    fs.readFile(file, options.encoding || 'utf8', function(err,data){
+        var defaultNamespace = null;
+        if (err) {
+            console.log("File read FAIL, error: " + err);
+            return callback(false, err);
+        }
+        console.log("File read ok, length: " + data.length);
+        var local = $rdf.uri.join(file, base) + '#'
+        var ns = options.ns || local
         var DOMParser = require('xmldom').DOMParser;
         var doc = new DOMParser().parseFromString(data);
-        var root = kb.bnode();
-        kb.add(kb.sum(uri), kb.sym('http://www.w3.org/2007/ont/link#xmlRoot'), root);
-        var convert = function(ele, node) {
-            if (ele.children) for(var i=0; i<ele.children.length; i++) {
-                var child = ele.children[i];
-                var node2 = kb.bnode();
-                kb.add(node, kb.sym(uri+child.tagName), node2)
+        var root = kb.sym(uri);
+
+        var justTextContent = function(ele) {
+            var ch = ele.childNodes;
+            var text = '';
+            if (ch) for (var i=0; i< ch.length; i++) {
+                if (ch[i].nodeType !== 3) {
+                    if (ch.length > 1 && options.iana && ch[i].nodeType === 1 && ch[i].nodeName === 'xref') {
+                        text += ' ' + ch[i].attributes.getNamedItem('data').nodeValue + ' ';
+                    } else {
+                        return false;
+                    }
+                } else {
+                    text += ch[i].nodeValue;
+                }
+            }
+
+            if (ele.attributes && ele.attributes.length > 0) {
+                return false;
+            }
+            if (!options.noTrim) {
+                text = text.trim();
+            }
+            return text;
+        }
+
+
+        var IANA_predicateMap = {
+            created: { uri: 'http://purl.org/dc/terms/created',
+                    type: 'http://www.w3.org/2001/XMLSchema#date'}, // @@CHECK
+            date: { uri: 'http://purl.org/dc/terms/date',
+                    type: 'http://www.w3.org/2001/XMLSchema#date'}, // @@CHECK
+            description: { uri: 'http://purl.org/dc/terms/description'}, // @@CHECK
+            value: { uri: 'http://www.w3.org/2000/01/rdf-schema#label'},
+            note:{ uri: 'http://www.w3.org/2000/01/rdf-schema#comment'}
+        };
+        var magicIANAxref = function(ele) {
+            var ch = ele.childNodes;
+            if (ch.length === 1 && ch[0].nodeName === 'xref') {
+                var at = ch[0].attributes;
+                var ty = at.getNamedItem('type').nodeValue;
+                var data = at.getNamedItem('data').nodeValue;
+                if (ty === 'uri') return kb.sym(data);
+                if (ty === 'rfc') return kb.sym('https://tools.ietf.org/html/' + data);
+                if (ty === 'draft') return kb.sym('https://tools.ietf.org/html/' + data);
                 
+            // RFCs are at e.g.  https://tools.ietf.org/html/rfc5005
+            // Internet Drafts are at e.g.
+            // https://tools.ietf.org/html/draft-ietf-httpbis-legally-restricted-status-04
+            
+            }
+            return null;
+        }
+
+        var magicIANAvalue = function(ele) {
+            var ch = ele.childNodes;
+            for (var i=0; i < ch.length; i++) {
+                if (ch[i].nodeName === 'value') { // a value subelement gives a local URI
+                    var localid = justTextContent(ch[i]);
+                    return kb.sym(local + localid);
+                }
+            }
+            return null;
+        }
+
+        var convert = function(ele, node, indent) {
+            indent = indent || '';
+            var pred, obj, type;
+            console.log(indent + "nodeName: " + ele.nodeName + " type " + ele.nodeType)
+            console.log(indent + "tagName: " + ele.tagName);
+            if (ele.nodeType in ignore) { // PI
+                return;
+            }
+            
+            var setPred = function(id) {
+                pred = kb.sym(ns + id);
+                if (options.predicateMap && options.predicateMap[id]) {
+                    var p = options.predicateMap[id];
+                    console.log(indent + "Mapping to " + p.uri)
+                    if (p.uri) {
+                        pred = kb.sym(p.uri);
+                    }
+                    if (p.type) {
+                        type = kb.sym(p.type);
+                    }
+                }
+            }
+                
+            if (ele.attributes) {
+                var attrs = ele.attributes, a;
+                console.log(indent + 'attributes: ' + attrs.length)
+                for (var j=0; j < attrs.length; j++) {
+                    a = attrs.item(j);
+                    console.log(indent + j + ") " +a.nodeName + " = " + a.nodeValue);
+                    if (a.nodeName === 'xmlns') {
+                        defaultNamespace = a.nodeValue;
+                
+                        if (defaultNamespace  == 'http://www.iana.org/assignments') {
+                            options.iana = true;
+                            ns = 'https://www.w3.org/ns/assignments/reg#';
+                            options.predicateMap = IANA_predicateMap;
+                            console.log('IANA MODE');
+                
+                        } else if (defaultNamespace === 'http://www.topografix.com/GPX/1/1') {
+                            ns ='http://hackdiary.com/ns/gps#'; // @@@ u
+                        }
+                        continue;
+                    }
+                    setPred(a.nodeName);
+                    kb.add(node, pred, $rdf.lit(a.nodeValue, undefined, type), targetDocument);
+                }
+            }
+            if (ele.childNodes) {
+                console.log(indent + "children " +ele.childNodes.length)
+                for(var i=0; i<ele.childNodes.length; i++) {
+                    console.log(indent + '  i ' + i)
+                    var child = ele.childNodes[i];
+                    type = null;
+                    setPred(child.nodeName);
+                    if (child.nodeType === 3) { // text
+                        //throw "We should not see text nodes at this level"
+                        console.log(indent + "  nodeName: " + child.nodeName + " type " + child.nodeType)
+                        obj = child.nodeValue.trim(); // @@ optional
+                        if (obj.length !== 0) {
+                            console.log($rdf.lit(obj, undefined, type))
+                            kb.add(node, kb.sym(ns + ele.nodeName), $rdf.lit(obj, undefined, type), targetDocument)
+                            console.log(indent + 'actual text ' + obj)
+                        } else {
+                            console.log(indent + 'whitespace')
+                        }
+                    } else if (!(child.nodeType in ignore)){
+                        var txt = justTextContent(child);
+                        if (txt !== false) {
+                            if (txt.length > 0) {
+                                kb.add(node, pred, $rdf.lit(txt, undefined, type), targetDocument)
+                                console.log($rdf.lit(txt, undefined, type))
+                            }
+                        } else if (options.iana && magicIANAxref(child)) {
+                            kb.add(node, kb.sym(ns + child.nodeName), magicIANAxref(child), targetDocument);
+                            console.log(indent + "Magic IANA xref " + magicIANAxref(child))
+                        } else {
+                            if (child.attributes && child.attributes.getNamedItem('id')){
+                                obj = kb.sym(local + child.attributes.getNamedItem('id').nodeValue);
+                            } else if (options.iana && magicIANAvalue(child)) {
+                                obj = magicIANAvalue(child);
+                                console.log(indent + "Magic IANA value " + obj)
+                                kb.add(obj, RDF('type'), RDF('Property') , targetDocument);
+                            } else {
+                                obj = kb.bnode();
+                            }
+                            kb.add(node, pred, obj, targetDocument);
+                            convert(child, obj, indent + '    ');
+                        }
+                    }
+               }
             }
         }
         convert(doc, root);
