414cfbd2b7e000b4bf7c1b388dfdc24c674c6b15
Eric Prud'hommeaux <eric@w3.org>
2017-10-24 09:15:58 -0400

    + test output

--- test/pwn-FHIR.sh
+++ test/pwn-FHIR.sh
@@ -1,5 +1,8 @@
-echo from-file/
+echo from-file
 mkdir -p from-file/ && (cd from-file/ && ../../rabel.js -in=../named-resources/observation-example-bloodpressure.ttl -spray=http://hl7.org/fhir/)
 
-echo from-http/
+echo from-http
 mkdir -p from-http/ && (cd from-http/ && ../../rabel.js -in=http://build.fhir.org/observation-example.ttl -spray=http://hl7.org/fhir/)
+
+echo doing stats
+find named-resources/observation-example-bloodpressure.ttl from-file from-http -type f | while read f; do (echo -n "$f - " && stat -c %s $f); done > pwn-FHIR.stats
